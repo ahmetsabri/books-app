6c777624c3a38f90e842a618daec59402b8b8781
ahmedsabry1996 <ahmedmahfouzjob@gmail.com>
2021-05-21 21:47:15 +0300

    Prepare Author Model

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -2,19 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AuthorSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            AuthorSeeder::class,
-        ]);
+        Author::factory()->count(10)->create();
     }
 }
